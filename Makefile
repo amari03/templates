024cfcd0681679dd71825e50e279d76139bafd70
Addie Vasquez <2020151434@ub.edu.bz>
2025-03-16 09:40:05 -0600

    finished with the database videos

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 ## Filename Makefile
 
+include .envrc
+
 .PHONY: run/tests
 run/tests: vet
 	go test -v ./...
@@ -14,4 +16,20 @@ vet: fmt
 
 .PHONY: run
 run: vet
-	go run ./cmd/web -addr=":4000"
+	go run ./cmd/web -addr=":4000" -dsn=${FEEDBACK_DB_DSN}
+
+.PHONY: db/psql
+db/psql:
+	psql ${FEEDBACK_DB_DSN}
+
+## db/migrations/new name=$1: create a new database migration
+.PHONY: db/migrations/new
+db/migrations/new:
+	@echo 'Creating migration files for ${name}...'
+	migrate create -seq -ext=.sql -dir=./migrations ${name}
+
+## db/migrations/up: apply all up database migrations
+.PHONY: db/migrations/up
+db/migrations/up:
+	@echo 'Running up migrations...'
+	migrate -path ./migrations -database ${FEEDBACK_DB_DSN} up
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"html/template"
 	"log/slog"
 	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
@@ -14,15 +19,25 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
+	addr := flag.String("addr", "", "HTTP network address")
+	dsn := flag.String("dsn", "", "PostgreSQL DSN")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	logger.Info("database connection pool established")
+
 	templateCache, err := newTemplateCache()
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+	defer db.Close()
 
 	app := &application{
 		logger:        logger,
@@ -35,4 +50,25 @@ func main() {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	// open a connection pool
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// set a context to ensure DB operations don't take too long
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	// return the connection pool (sql.DB)
+	return db, nil
+
 }
